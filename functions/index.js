aab7fc2a0067e718f7f735df5394b3b246dfa02a
Mani Jafari <manielli@gmail.com>
2019-03-18 17:26:37 -0700

    Created a firebase project, added a create_user google cloud function file, with a simple echoing function

---
+++ functions/index.js
@@ -0,0 +1,15 @@
+const functions = require('firebase-functions');
+const createUser = require('./create_user');
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+exports.helloWorld = functions.https.onRequest((request, response) => {
+ response.send("Hello from Firebase!");
+});
+
+exports.goodbye = functions.https.onRequest((request, response) => {
+    response.send("Goodbye!");
+});
+
+exports.createUser = functions.https.onRequest(createUser);
