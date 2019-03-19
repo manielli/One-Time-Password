223025b28c2099f423a128a528fb165b34f84945
Mani Jafari <manielli@gmail.com>
2019-03-18 17:40:09 -0700

    Writing the create_user google cloud function, in 4 steps, step 1 completed

--- functions/create_user.js
+++ functions/create_user.js
@@ -1,3 +1,14 @@
+cons admin = require('firebase-admin');
+
 module.exports = function(req, res) {
-    res.send(req.body);
+    // Verify the user provided a phone number
+    if (!req.body.phone) {
+        return res.status(422).send({ error: 'Bad Input' });
+    }
+
+    // Fromat the phone number to remove dashes and parentheses'
+
+    // Create a new user account using that phone number
+
+    // Respond to the user request, saying the account was made
 };
