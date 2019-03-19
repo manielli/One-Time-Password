17dfe8b48bf2de932242f1a77aceb210384f9c8d
Mani Jafari <manielli@gmail.com>
2019-03-18 18:10:54 -0700

    Fixed a typo and some shorthand syntax

--- functions/create_user.js
+++ functions/create_user.js
@@ -1,4 +1,4 @@
-cons admin = require('firebase-admin');
+const admin = require('firebase-admin');
 
 module.exports = function(req, res) {
     // Verify the user provided a phone number
@@ -11,8 +11,8 @@ module.exports = function(req, res) {
     
     // Create a new user account using that phone number
     admin.auth().createUser({ uid: phone })
-        .then((user) => { res.send(user) })
-        .catch((err) => { res.status(422).send({ error: err }) });
+        .then(user => res.send(user))
+        .catch(err => res.status(422).send({ error: err }));
 
     // Respond to the user request, saying the account was made
 };
